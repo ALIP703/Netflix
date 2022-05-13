1aac646eecaf8b24128b732b9ab3fa14682e3b39
muhammed ali <muhammedalip703@gmail.com>
2022-05-13 12:15:05 +0530

    styling

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import NavBar from './components/navbar/NavBar';
+import Banner from './components/Banner/Banner';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <Banner/>
+    </div>
+  );
+}
+
+export default App;
